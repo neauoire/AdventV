e2339bf4cd3fb904a91d65919c6effdfad87e03e
Devine Lu Linvega <aliceffekt@gmail.com>
2016-06-08 14:28:24 -0700

    Added new events

--- class.world.rb
+++ class.world.rb
@@ -50,6 +50,7 @@ class World
 		location.addEvent(Event.new(4, Encounter.new("fought a thief at",5), itemWithPower(2) ))
 		# Passive
 		location.addEvent(Event.new(nil, Encounter.new("drinking ale at",-1), nil))
+		location.addEvent(Event.new(nil, Encounter.new("fought a drunk in",2), nil))
 		return location
 
 	end
@@ -60,7 +61,8 @@ class World
 		# Battle 2
 		location.addEvent(Event.new(5, Encounter.new("battled a mercenary at",4), nil))
 		# Passive
-		location.addEvent(Event.new(nil, Encounter.new("defending",0), itemWithPower(1) ))
+		location.addEvent(Event.new(nil, Encounter.new("defended",0), itemWithPower(1) ))
+		location.addEvent(Event.new(nil, Encounter.new("met the queen of",-2), nil))
 		return location
 
 	end
@@ -72,6 +74,7 @@ class World
 		location.addEvent(Event.new(6, Encounter.new("fighting a slug at",3), nil))
 		# Passive
 		location.addEvent(Event.new(nil, Encounter.new("swimming in",-1), nil))
+		location.addEvent(Event.new(nil, Encounter.new("punched a serpent at",3), nil))
 		return location
 
 	end
@@ -81,9 +84,10 @@ class World
 		location = Location.new("forest")
 		location.addPicture
 		# Battle 3
-		location.addEvent(Event.new(4, Encounter.new("fought a faery in",5), itemWithPower(3) ))
+		location.addEvent(Event.new(4, Encounter.new("fought a ghost in",5), itemWithPower(3) ))
 		# Passive
 		location.addEvent(Event.new(nil, Encounter.new("hunting in",0), itemWithPower(2) ))
+		location.addEvent(Event.new(nil, Encounter.new("steps in a bee's nest in",3), nil))
 		return location
 
 	end
@@ -92,11 +96,12 @@ class World
 
 		location = Location.new("camp")
 		# Boss
-		location.addEvent(Event.new(5, Encounter.new("assaulted the knight at",6), nil))
+		location.addEvent(Event.new(5, Encounter.new("assaulted a knight at",6), nil))
 		# Battle 1
 		location.addEvent(Event.new(6, Encounter.new("fighting a soldier at",3), nil))
 		# Passive
 		location.addEvent(Event.new(nil, Encounter.new("sleeping at",-2), nil ))
+		location.addEvent(Event.new(nil, Encounter.new("met a wolf",-2), nil))
 		return location
 
 	end
@@ -109,7 +114,7 @@ class World
 		# Passive
 		location.addEvent(Event.new(nil, Encounter.new("swatted a bat in",2), nil))
 		location.addEvent(Event.new(nil, Encounter.new("batted a rat in",3), nil))
-		location.addEvent(Event.new(nil, Encounter.new("descending into",0), itemWithPower(3) ))
+		location.addEvent(Event.new(nil, Encounter.new("descended into",0), itemWithPower(3) ))
 		return location
 
 	end
@@ -121,6 +126,7 @@ class World
 		location.addEvent(Event.new(4, Encounter.new("fought an elemental by",5), itemWithPower(4) ))
 		# Passive
 		location.addEvent(Event.new(nil, Encounter.new("mud-bathing in",-2), nil ))
+		location.addEvent(Event.new(nil, Encounter.new("encountered the witch of",3), nil))
 		return location
 
 	end
@@ -137,6 +143,7 @@ class World
 		location.addEvent(Event.new(4, Encounter.new("fought a guard in",6), itemWithPower(3) ))
 		# Passive
 		location.addEvent(Event.new( nil, Encounter.new("drinking wine in",-3), nil))
+		location.addEvent(Event.new(nil, Encounter.new("batted a drake",7), nil))
 		return location
 
 	end
@@ -149,6 +156,7 @@ class World
 		location.addEvent(Event.new(5, Encounter.new("battled a worm in",5), nil))
 		# Passive
 		location.addEvent(Event.new(nil, Encounter.new("reaching",0), itemWithPower(4) ))
+		location.addEvent(Event.new(nil, Encounter.new("fought a vampire in",6), nil))
 		return location
 
 	end
@@ -162,6 +170,7 @@ class World
 		location.addEvent(Event.new(6, Encounter.new("fighting a nomad in",4), nil))
 		# Passive
 		location.addEvent(Event.new(nil, Encounter.new("drinking in",-3), nil))
+		location.addEvent(Event.new(nil, Encounter.new("batted a cadaver in",4), nil))
 		return location
 
 	end
@@ -173,6 +182,7 @@ class World
 		location.addEvent(Event.new(4, Encounter.new("fought a sandman in",6), itemWithPower(4) ))
 		# Passive
 		location.addEvent(Event.new(nil, Encounter.new("stranded in",0), itemWithPower(5) ))
+		location.addEvent(Event.new(nil, Encounter.new("found the oasis in",-3), nil))
 		return location
 
 	end
@@ -181,9 +191,10 @@ class World
 
 		location = Location.new("monolith")
 		# Battle 1
-		location.addEvent(Event.new(6, Encounter.new("fighting a bat by",4), nil))
+		location.addEvent(Event.new(6, Encounter.new("fought a bat by",4), nil))
 		# Passive
-		location.addEvent(Event.new(nil, Encounter.new("praying by",-4), nil ))
+		location.addEvent(Event.new(nil, Encounter.new("prayed by",-4), nil ))
+		location.addEvent(Event.new(nil, Encounter.new("met god near",8), nil))
 		return location
 
 	end
@@ -192,9 +203,10 @@ class World
 
 		location = Location.new("crypt")
 		# Battle 2
-		location.addEvent(Event.new(5, Encounter.new("battled a vampire at",5), nil))
+		location.addEvent(Event.new(5, Encounter.new("battled a vampire at",6), nil))
 		# Passive
-		location.addEvent(Event.new(nil, Encounter.new("exploring",0), itemWithPower(6) ))
+		location.addEvent(Event.new(nil, Encounter.new("explored",0), itemWithPower(6) ))
+		location.addEvent(Event.new(nil, Encounter.new("batted a zombie in",4), nil))
 		return location
 
 	end
@@ -205,7 +217,8 @@ class World
 		# Battle 3
 		location.addEvent(Event.new(4, Encounter.new("fought a traveler at",6), itemWithPower(5) ))
 		# Passive
-		location.addEvent(Event.new(nil, Encounter.new("shopping at",-4), nil ))
+		location.addEvent(Event.new(nil, Encounter.new("shopped at",-4), nil ))
+		location.addEvent(Event.new(nil, Encounter.new("fought wanderers by",3), nil))
 		return location
 
 	end
@@ -221,7 +234,7 @@ class World
 		# Battle 3
 		location.addEvent(Event.new(4, Encounter.new("fought the prince at",7), itemWithPower(4) ))
 		# Passive
-		location.addEvent(Event.new(nil, Encounter.new("drinking moonshine",-5), nil))
+		location.addEvent(Event.new(nil, Encounter.new("drank moonshine",-5), nil))
 		return location
 
 	end
@@ -233,7 +246,8 @@ class World
 		# Battle 2
 		location.addEvent(Event.new(5, Encounter.new("battled an undead at",6), nil))
 		# Passive
-		location.addEvent(Event.new(nil, Encounter.new("exploring",0), itemWithPower(7) ))
+		location.addEvent(Event.new(nil, Encounter.new("explored",0), itemWithPower(7) ))
+		location.addEvent(Event.new(nil, Encounter.new("wandering in",3), nil))
 		return location
 
 	end
@@ -242,11 +256,11 @@ class World
 
 		location = Location.new("fountain")
 		# Boss
-		location.addEvent(Event.new(5, Encounter.new("assaulted the merfolk",7), itemWithPower(5) ))
+		location.addEvent(Event.new(5, Encounter.new("assaulted the merfolks",7), itemWithPower(5) ))
 		# Battle 1
-		location.addEvent(Event.new(6, Encounter.new("fighting a leviathan in",5), nil))
+		location.addEvent(Event.new(6, Encounter.new("fought a leviathan in",5), nil))
 		# Passive
-		location.addEvent(Event.new(nil, Encounter.new("drinking from",-5), nil))
+		location.addEvent(Event.new(nil, Encounter.new("drank from",-5), nil))
 		return location
 
 	end
@@ -257,7 +271,8 @@ class World
 		# Battle 3
 		location.addEvent(Event.new(4, Encounter.new("fought a goblin",7), nil))
 		# Passive
-		location.addEvent(Event.new(nil, Encounter.new("excavating",0), itemWithPower(8) ))
+		location.addEvent(Event.new(nil, Encounter.new("excavated",0), itemWithPower(8) ))
+		location.addEvent(Event.new(nil, Encounter.new("batted a tunneler in",6), nil))
 		return location
 
 	end
@@ -266,9 +281,11 @@ class World
 
 		location = Location.new("abyss")
 		# Battle 1
-		location.addEvent(Event.new(6, Encounter.new("fighting an imp in",5), nil))
+		location.addEvent(Event.new(6, Encounter.new("fighting the imp in",5), itemWithPower(7)))
 		# Passive
 		location.addEvent(Event.new(nil, Encounter.new("finding food in",-6), nil ))
+		location.addEvent(Event.new(nil, Encounter.new("fought the gatekeeper in",11), nil))
+		location.addEvent(Event.new(nil, Encounter.new("lost",-5), nil))
 		return location
 
 	end
@@ -280,6 +297,7 @@ class World
 		location.addEvent(Event.new(5, Encounter.new("battled a demon at",6), nil))
 		# Passive
 		location.addEvent(Event.new(nil, Encounter.new("opening a tomb in",0), itemWithPower(9) ))
+		location.addEvent(Event.new(nil, Encounter.new("batted a fish in",1), nil))
 		return location
 
 	end
@@ -288,9 +306,10 @@ class World
 
 		location = Location.new("portal")
 		# Battle 3
-		location.addEvent(Event.new(4, Encounter.new("fought a god",7), itemWithPower(6) ))
+		location.addEvent(Event.new(4, Encounter.new("fought a small god",7), itemWithPower(6) ))
 		# Passive
-		location.addEvent(Event.new(nil, Encounter.new("traversing",-6), nil ))
+		location.addEvent(Event.new(nil, Encounter.new("traversed",-6), nil ))
+		location.addEvent(Event.new(nil, Encounter.new("batted an orb",3), nil))
 		return location
 
 	end
