4826903d5e51e7178ae26e39d17457590fec8ec5
Devine Lu Linvega <aliceffekt@gmail.com>
2016-06-08 12:17:46 -0700

    Began migrating to Jiin

---
+++ lamp.rb
@@ -0,0 +1,21 @@
+#!/bin/env ruby
+
+require 'json'
+require 'date'
+require 'rubygems'
+
+class Adventv
+
+	include Lamp
+
+	def initialize query = nil
+
+	end
+
+	def application query = nil
+
+		return query+"???"
+
+	end
+
+end
